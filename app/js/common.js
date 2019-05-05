445334342e3e68ea632d43506c2c52084b4e816a
rudolif <rudolifrudolif@gmail.com>
2019-05-05 15:34:58 +0300

    two commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,58 @@
 $(function() {
 
+
+
+//-------------------------------анімація цифр---------------------------------------
+
+if($('*').is('#about')) {
+  var show = true;
+  var countbox = ".about";
+  $(window).on("scroll load resize", function () {
+      if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+      var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+      var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+      var w_height = $(window).height(); // Высота окна браузера
+      var d_height = $(document).height(); // Высота всего документа
+      var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+      if (w_top + 810 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+          $('.about__list_item h3').spincrement({
+              thousandSeparator: "",
+              duration: 6000
+          });
+          show = false;
+      }
+  });
+}
+
+
+
+//-----------------------------training-slider-----------------------------
+  $('.training__slider').slick({
+    centerMode: true,
+    dots: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
+  });
+
 //------------------------------гамбургер-----------------------------
   $('.hamburger').click(function() {
     $(this).toggleClass('hamburger--active');
@@ -10,6 +63,54 @@ $(function() {
 //-------------------------------попандер---------------------------------------
   $('.modal').popup({transition: 'all 0.3s'});
 
+//------------------------------------form-------------------------------------------
+  $('input[type="tel"]').mask('+0 (000) 000-00-00');
+
+  jQuery.validator.addMethod("phoneno", function(phone_number, element) {
+     return this.optional(element) || phone_number.match(/\+[0-9]{1}\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}/);
+  }, "Введите Ваш телефон");
+
+  $(".form").each(function(index, el) {
+    $(el).addClass('form-' + index);
+
+    $('.form-' + index).validate({
+      rules: {
+        phone: {
+          required: true,
+          phoneno: true
+        },
+        name: 'required',
+      },
+      messages: {
+        name: "Введите Ваше имя",
+        phone: "Введите Ваш телефон",
+      },
+      submitHandler: function(form) {
+        var t = {
+          name: jQuery('.form-' + index).find("input[name=name]").val(),
+          phone: jQuery('.form-' + index).find("input[name=phone]").val(),
+          subject: jQuery('.form-' + index).find("input[name=subject]").val()
+        };
+        ajaxSend('.form-' + index, t);
+      }
+    });
+
+  });
+
+  function ajaxSend(formName, data) {
+    jQuery.ajax({
+      type: "POST",
+      url: "sendmail.php",
+      data: data,
+      success: function() {
+        $(".modal").popup("hide");
+        $("#thanks").popup("show");
+        setTimeout(function() {
+          $(formName).trigger('reset');
+        }, 2000);
+      }
+    });
+  }
 
 //----------------------------------------fixed----------------------------------
   $(window).scroll(function(){
@@ -22,11 +123,76 @@ $(function() {
   });
 
 //-------------------------скорость якоря---------------------------------------
-  $(".header__list").on("click","a", function (event) {
+  $(".click").on("click","a", function (event) {
       event.preventDefault();
       var id  = $(this).attr('href'),
           top = $(id).offset().top;
-      $('body,html').animate({scrollTop: top - 60}, 'slow', 'swing');
+      $('body,html').animate({scrollTop: top - 105}, 'slow', 'swing');
   });
+
   
 });
+
+//----------------------------------------preloader----------------------------------
+
+  $(window).on('load', function(){
+    $('.preloader').delay(2500).fadeOut('slow');
+  });
+
+//--------------------------------------js----------------------------------------
+
+;( function( window, document )
+{
+  'use strict';
+
+  var file     = 'img/symbols.html',
+      revision = 1.2;
+
+  if( !document.createElementNS || !document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ).createSVGRect )
+      return true;
+
+  var isLocalStorage = 'localStorage' in window && window[ 'localStorage' ] !== null,
+      request,
+      data,
+      insertIT = function()
+      {
+          document.body.insertAdjacentHTML( 'afterbegin', data );
+      },
+      insert = function()
+      {
+          if( document.body ) insertIT();
+          else document.addEventListener( 'DOMContentLoaded', insertIT );
+      };
+
+  if( isLocalStorage && localStorage.getItem( 'inlineSVGrev' ) == revision )
+  {
+    data = localStorage.getItem( 'inlineSVGdata' );
+    if( data )
+    {
+        insert();
+        return true;
+    }
+  }
+
+  try
+  {
+    request = new XMLHttpRequest();
+    request.open( 'GET', file, true );
+    request.onload = function()
+      {
+        if( request.status >= 200 && request.status < 400 )
+          {
+            data = request.responseText;
+            insert();
+            if( isLocalStorage )
+            {
+              localStorage.setItem( 'inlineSVGdata',  data );
+              localStorage.setItem( 'inlineSVGrev',   revision );
+            }
+        }
+    }
+    request.send();
+  }
+  catch( e ){}
+
+}( window, document ) );
